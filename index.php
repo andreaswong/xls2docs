9fd6e03d26abf86bd769b12ca2d4fef97eb5208e
andreas <andreas@eatigo.com>
2018-06-29 13:19:53 +0800

    master removed vendor

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
-exit('aa');
 require_once 'routes.php';
--- routes.php
+++ routes.php
@@ -6,6 +6,7 @@ $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
 $klein = new \Klein\Klein();
+echo 'Using route base ' . getenv('BASE_PATH') . '<br>';
 $klein->with('/' . getenv('BASE_PATH'), function() use ($klein) {
 	$klein->respond('GET', '/', function ($request, $response, $service, $app) use ($klein) {
 		$service->render('xls.php');
