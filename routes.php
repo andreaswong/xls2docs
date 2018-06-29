5bfa570b54bd01ad0b9d4defa73c1bcedf7c5ca4
andreas <andreas@eatigo.com>
2018-06-29 13:30:16 +0800

    master removed vendor

--- routes.php
+++ routes.php
@@ -6,15 +6,13 @@ $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
 $klein = new \Klein\Klein();
-$klein->with('/' . getenv('BASE_PATH'), function() use ($klein) {
-	$klein->respond('GET', '/', function ($request, $response, $service, $app) use ($klein) {
-		$service->render('xls.php');
-	});
+$klein->respond('GET', '/', function ($request, $response, $service, $app) use ($klein) {
+	$service->render('xls.php');
+});
 
-	$klein->respond('POST', '/', function ($request, $response, $service, $app) use ($klein) {
-		$zip = handleUpload($request->files()['xls-file']['tmp_name']);
-		$response->file($zip);
-	});
+$klein->respond('POST', '/', function ($request, $response, $service, $app) use ($klein) {
+	$zip = handleUpload($request->files()['xls-file']['tmp_name']);
+	$response->file($zip);
 });
 
 $klein->dispatch();
